230807a0eb10a05d3c2b4895676677a043b903de
tiddy41 <57396074+tiddy41@users.noreply.github.com>
2020-01-16 19:01:31 +0900

    @item

--- app/controllers/my_page_controller.rb
+++ app/controllers/my_page_controller.rb
@@ -1,7 +1,8 @@
 class MyPageController < ApplicationController
   before_action :authenticate_user!
   def index
-    
+    binding.pry
+    @items = Item.where(user_id: current_user.id)
   end
   
 end
